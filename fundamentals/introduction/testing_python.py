3071bd3308862999b8b9004ee3fb86c89c09ba04
Benji Hix <benji.hix@outlook.com>
2023-08-27 18:03:41 -0700

    initialize new repo

---
+++ fundamentals/introduction/testing_python.py
@@ -0,0 +1,15 @@
+x = 10
+if x > 50:
+    print("bigger than 50")
+elif x == 10:
+    pass
+else:
+    print("smaller than 50")
+print('code was passed')
+
+tuple = (1, 2, 3)
+list = ['list1', 'list2', 'list3']
+dictionary = {'first': 'dictionary1', 'second': 'dictionary2', 'third': 'dictionary3'}
+
+name = "benji"
+print("My name is", name)
