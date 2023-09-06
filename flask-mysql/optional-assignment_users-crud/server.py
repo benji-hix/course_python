230c0850b30994eef469cc065f57601d58f927c1
Benji Hix <benji.hix@outlook.com>
2023-09-06 14:50:54 -0700

    completed optional assignment users crud

---
+++ flask-mysql/optional-assignment_users-crud/server.py
@@ -0,0 +1,49 @@
+from flask import render_template, Flask, request, redirect, session
+from user import User
+
+app = Flask(__name__)
+app.secret_key = "yoshi yoshi yoshi"
+
+@app.route('/users')
+def index():
+
+    all_users = User.get_all()
+    return render_template('read.html', all_users = all_users)
+
+@app.route('/users/new')
+def users_create():
+    input_phrase = 'Add A New User'
+    return render_template('create.html', input_phrase = input_phrase)
+
+@app.route('/submit_new', methods = ['POST'])
+def users_submit_new():
+    new_user_id = User.create(request.form)
+    redirect_url = '/users/' + str(new_user_id)
+    return redirect(redirect_url)
+
+@app.route ('/users/<int:id>')
+def users_read(id):
+    input_phrase = 'User '
+    user = User.read(id)
+    return render_template('user_page.html', input_phrase = input_phrase, display_read = 'static', display_update = 'none', user = user)
+
+@app.route('/users/<int:id>/update')
+def users_update(id):
+    input_phrase = 'Edit user info '
+    user = User.read(id)
+    return render_template('user_page.html', input_phrase = input_phrase, user = user, display_read = 'none', display_update = 'static' )
+
+# ---------------------------------------------------------------------------- #
+@app.route('/submit_update', methods = ['POST'])
+def users_submit_update():
+    user = User.update(request.form)
+    redirect_url = '/users/' + request.form['form_id']
+    return redirect(redirect_url)
+# ---------------------------------------------------------------------------- #
+@app.route('/users/<int:id>/delete')
+def users_delete(id):
+    User.delete(id)
+    return redirect('/users')
+
+if __name__ == '__main__':
+    app.run(debug=True)
