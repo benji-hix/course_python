7217d4f84e1c20100bd1f8d7ccaa7ed0d397801e
Benji Hix <benji.hix@outlook.com>
2023-09-07 14:57:39 -0700

    cleanup

--- flask-mysql/assignment_dojos-and-ninjas/server.py
+++ flask-mysql/assignment_dojos-and-ninjas/server.py
@@ -2,4 +2,9 @@ from flask_app.controllers import dojos, ninjas
 from flask_app import app
 
 if __name__ == '__main__':
-    app.run(debug = True)
+    app.run(debug = True) 
+
+#! Alert comment
+#? Question!
+#// strikethrough comment
+#* Important Info
